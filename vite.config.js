5aa32e4377ebbf80c70bebafe3ba25c460589bb2
Brent Self <brentself@gmail.com>
2022-09-23 09:22:11 -0600

    vite config updated with production base url

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'development'
+    ? '/'
+    : '/web-audio/synth/',
   plugins: [vue()],
   resolve: {
     alias: {
